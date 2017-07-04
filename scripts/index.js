130c9154b4548db43237f97830ba3bac9355b6f2
david robison <mrdavidrobison@gmail.com>
2017-07-03 18:14:33 -0600

    no more light colored classes in js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
 
-  $("#lightRed, #lightBlue, #lightYellow, #lightGreen").hide();
-  $("#red").click(function(){
-    $("#red").hide();
-    $("#lightRed").show();
-  })
+  
 
 })

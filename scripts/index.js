f8fcd8f54fb0277f122af1f6d15f327bbda10b65
david robison <mrdavidrobison@gmail.com>
2017-07-03 18:04:53 -0600

    show and hide red figure when clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
   $("#lightRed, #lightBlue, #lightYellow, #lightGreen").hide();
+  $("#red").click(function(){
+    $("#red").hide();
+    $("#lightRed").show();
+  })
 
 })

7d6ad63b0c0f49141a99fa8f13e573afbcc2c398
david robison <mrdavidrobison@gmail.com>
2017-07-09 17:47:33 -0600

    edit var random

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
   var colorWheel = [];
-  var random = Math.random() * (4 - 1) + 1;
+  var random = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
 
   $("#lightGreen").hide();
   $("#lightRed").hide();
   $("#lightBlue").hide();
   $("#lightYellow").hide();
 
-  $("#replay").click(function(){
+  $("#reset").click(function(){
     var colorWheel = [];
   })
 
@@ -19,17 +19,17 @@ $(document).ready(function(){
       $("#lightRed").fadeIn(400);
       $("#lightRed").fadeOut(400);
       $("#red").fadeIn();
-    } else if (colorWheel[colorWheel.length - 1] = 1) {
+    } else if (colorWheel[colorWheel.length - 1] = 2) {
       $("#blue").fadeOut(400);
       $("#lightBlue").fadeIn(400);
       $("#lightBlue").fadeOut(400);
       $("#blue").fadeIn();
-    } else if (colorWheel[colorWheel.length - 1] = 1) {
+    } else if (colorWheel[colorWheel.length - 1] = 3) {
       $("#yellow").fadeOut(400);
       $("#lightYellow").fadeIn(400);
       $("#lightYellow").fadeOut(400);
       $("#yellow").fadeIn();
-    } else if (colorWheel[colorWheel.length - 1] = 1) {
+    } else if (colorWheel[colorWheel.length - 1] = 4) {
       $("#green").fadeOut(400);
       $("#lightGreen").fadeIn(400);
       $("#lightGreen").fadeOut(400);
